b944be912e6c32cf55b75c153d9fcab9209299fc
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-05-26 20:39:19 +0100

    trying some changes from original auth code

--- server.js
+++ server.js
@@ -33,7 +33,55 @@ app.get('/callback', function(req, res) {
       ).toString('base64'))
     },
     json: true
-  }
+}
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+
+      var access_token = body.access_token,
+          refresh_token = body.refresh_token;
+
+      var options = {
+        url: 'https://api.spotify.com/v1/me',
+        headers: { 'Authorization': 'Bearer ' + access_token },
+        json: true
+      };
+
+      // use the access token to access the Spotify Web API
+      request.get(options, function(error, response, body) {
+        console.log(body);
+      });
+
+      // we can also pass the token to the browser to make requests from there
+      res.redirect('https://spotify-mosaic-backend.herokuapp.com/#' +
+        querystring.stringify({
+          access_token: access_token,
+          refresh_token: refresh_token
+        }));
+    } else {
+      res.redirect('/#' +
+        querystring.stringify({
+          error: 'invalid_token'
+        }));
+    }
+  });
+}
+);
+
+app.get('/refresh_token', function(req, res) {
+
+// requesting access token from refresh token
+var refresh_token = req.query.refresh_token;
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+  form: {
+    grant_type: 'refresh_token',
+    refresh_token: refresh_token
+  },
+  json: true
+};
+
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token
     let uri = process.env.FRONTEND_URI || 'https://spotify-mosaic.now.sh/'
